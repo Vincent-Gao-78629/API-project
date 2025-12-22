b934f683ffef2af0a3fbd41a6d80f9d71c366ad3
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-12-22 12:32:29 -0500

    kms

--- API/src/main.js
+++ API/src/main.js
@@ -1,3 +1,19 @@
 import "./style.css";
 
-get;
+const apiCall = "https://www.freetogame.com/api/games";
+
+async function getGames(apiCall) {
+  try {
+    const response = await fetch(apiCall);
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getGames(apiCall);
