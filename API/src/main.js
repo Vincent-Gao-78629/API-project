488161e550623a4c3cc28d9d3366e75c399e2afc
Vincent-Gao-78629 <vgao78629@gmail.com>
2025-12-23 11:49:23 -0500

    xs

--- API/src/main.js
+++ API/src/main.js
@@ -5,9 +5,13 @@ const URL = "https://dogapi.dog/api/v1/facts";
 async function fetchData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data.facts);
-    document.querySelector("#apiData").textContent = data.facts;
+    if (!response.ok) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data.facts);
+      document.querySelector("#apiData").textContent = data.facts;
+    }
   } catch (error) {
     console.log(error);
   }
