a1ab736b21ec2a2cb52d1f24071fc2a93b4910c2
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-01-06 12:09:36 -0500

    nothing here

--- API/src/main.js
+++ API/src/main.js
@@ -1,19 +1,24 @@
 import "./style.css";
 
-const URL = "https://www.freetogame.com/api/games";
+const URL = "https://api.pokemontcg.io/v2/cards";
+const API_KEY = "af71b87a-fc23-45fd-90ee-e55622ff1404";
 
-async function fetchData(URL) {
+async function fetchData() {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(URL, {
+      headers: {
+        "X-Api-Key": API_KEY,
+      },
+    });
+
     if (!response.ok) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      document.querySelector("#apiData").textContent = data;
+      throw new Error(`HTTP error: ${response.status}`);
     }
+    const data = await response.json();
+    document.querySelector("#apiData").textContent = data.card[0].name;
   } catch (error) {
-    console.log(error);
+    console.error(error.message);
   }
 }
 
-fetchData(URL);
+fetchData();
