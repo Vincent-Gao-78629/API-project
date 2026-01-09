8f1c25ac3b6564f866eb017d7ca9fbd79338dd6f
Vincent-Gao-78629 <vgao78629@gmail.com>
2026-01-09 06:14:12 -0500

    bh

--- API/src/main.js
+++ API/src/main.js
@@ -1,11 +1,13 @@
 import "./style.css";
 
-const URL =
+const URLSearch =
   "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=";
-let SearchQuery = "a"; //let them enter a query
-async function fetchData(URL, SearchQuery) {
+
+let SearchURL = "flowers";
+
+async function fetchIDWithImages(URL, SearchURL) {
   try {
-    const response = await fetch(`${URL}${SearchQuery}`);
+    const response = await fetch(`${URL}${SearchURL}`);
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
@@ -16,4 +18,32 @@ async function fetchData(URL, SearchQuery) {
   }
 }
 
-fetchData(URL, SearchQuery);
+fetchIDWithImages(URL, SearchURL);
+
+const ObjectIDSearch =
+  "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+async function fetchData(URL, SearchURL) {
+  try {
+    const response = await fetch(`${URL}${SearchURL}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    document.getElementById("apiData").textContent = data.objectIDs; //after getting object ids put the id->primary image on screen
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function fetchData(URL, SearchURL) {
+  try {
+    const response = await fetch(`${URL}${SearchURL}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    const data = await response.json();
+    document.getElementById("apiData").textContent = data.objectIDs; //after getting object ids put the id->primary image on screen
+  } catch (error) {
+    console.error(error.message);
+  }
+}
